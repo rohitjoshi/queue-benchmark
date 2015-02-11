e56de74d2b834934ddb5a1480fb410f3119641c9
Rohit Joshi <rohit.c.joshi@gmail.com>
2015-02-11 16:53:33 -0500

    initial version of benchmark test for boost lockfree and concurrentqueue

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=queue-benchmark
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/queue-benchmark
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=queue-benchmark.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/queue-benchmark.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=queue-benchmark
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/queue-benchmark
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=queue-benchmark.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/queue-benchmark.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
